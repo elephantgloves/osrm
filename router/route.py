67adc637bec86150e1ae4603e407a0841abbdae8
ykita <tachibana.yki@gmail.com>
2018-07-16 15:19:06 +0900

    Python 3 implementation

--- router/route.py
+++ router/route.py
@@ -8,11 +8,11 @@ class RouteObject(object):
         self.traveltime = routejson[0]["duration"]
         self.waypoints = [step["maneuver"]["location"] for step in routejson[0]["legs"][0]["steps"]]
 
-    def parseRoute(self):
+    def parse_route(self):
         return [self.length, self.traveltime, self.waypoints]
 
-    def totalRouteLength(self):
+    def total_route_length(self):
         return self.length
 
-    def getLegDetails(self):
+    def get_leg_details(self):
         pass
